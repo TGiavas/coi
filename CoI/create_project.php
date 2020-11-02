8c72104161382da1082a7f76d5aaa310981ac0f0
TGiavas <tgiavasoglou@gmail.com>
2020-11-02 22:19:33 +0200

    Add files via upload

--- CoI/create_project.php
+++ CoI/create_project.php
@@ -3,8 +3,8 @@
 require_once "connect.php";
 
 // Define variables and initialize with empty values
-$project_name = $project_desc = $public_servants = "";
-$project_name_err = $project_desc_err = $public_servants_err = "";
+$project_name = $project_start_date = $project_end_date = $project_desc = "";
+$project_name_err = $project_start_date_err = $project_end_date_err = $project_desc_err = "";
 
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -16,6 +16,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $project_name = $input_project_name;
     }
 
+    // Validate start date
+    $input_project_start_date = trim($_POST["project_start_date"]);
+    if (empty($input_project_start_date)) {
+        $project_start_date = "Please enter the project start date.";
+    } else {
+        $project_start_date = $input_project_start_date;
+    }
+
+    // Validate end date
+    $input_project_end_date = trim($_POST["project_end_date"]);
+    if (empty($input_project_end_date)) {
+        $project_end_date = "Please enter the project end date.";
+    } else {
+        $project_end_date = $input_project_end_date;
+    }
+    
     // Validate project_des
     $input_project_desc = trim($_POST["project_desc"]);
     if (empty($input_project_desc)) {
@@ -24,28 +40,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $project_desc = $input_project_desc;
     }
 
-    // Validate usertype
-    $input_public_servants = trim($_POST["public_servants"]);
-    if (empty($input_public_servants)) {
-        $public_servants_err = "Please enter the public servants.";
-    } else {
-        $public_servants = $input_public_servants;
-    }
+    // // Validate usertype
+    // $input_public_servants = trim($_POST["public_servants"]);
+    // if (empty($input_public_servants)) {
+    //     $public_servants_err = "Please enter the public servants.";
+    // } else {
+    //     $public_servants = $input_public_servants;
+    // }
 
 
     // Check input errors before inserting in database
-    if (empty($project_name_err) && empty($project_desc_err) && empty($public_servants_err) ){
+    if (empty($project_name_err) && empty($project_desc_err) && empty($project_start_date_err) && empty($project_end_date_err) ){
         // Prepare an insert statement
-        $sql = "INSERT INTO projects (project_name, project_desc, public_servants) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO projects (project_name, project_start_date, project_end_date, project_desc) VALUES (?, ?, ?, ?)";
 
         if ($stmt = mysqli_prepare($conn, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_project_name, $param_project_desc, $param_public_servants);
+            mysqli_stmt_bind_param($stmt, "ssss", $param_project_name, $param_project_start_date, $param_project_end_date, $param_project_desc);
 
             // Set parameters
             $param_project_name = $project_name;
+            $param_project_start_date = $project_start_date;
+            $param_project_end_date = $project_end_date;
             $param_project_desc = $project_desc;
-            $param_public_servants = $public_servants;
+            
+
 
             // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
@@ -73,6 +92,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <title>Create Record</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
     <style type="text/css">
         .wrapper {
             width: 500px;
@@ -96,15 +116,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <input type="text" name="project_name" class="form-control" value="<?php echo $project_name; ?>">
                             <span class="help-block"><?php echo $project_name_err; ?></span>
                         </div>
+                        <div class="form-group <?php echo (!empty($project_start_date_err)) ? 'has-error' : ''; ?>">
+                            <label>Project Start Date</label>
+                            <input type="date" name="project_start_date" class="form-control" value="<?php echo $project_start_date; ?>">
+                            <span class="help-block"><?php echo $project_start_date_err; ?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($project_end_date_err)) ? 'has-error' : ''; ?>">
+                            <label>Project End Date</label>
+                            <input type="date" name="project_end_date" class="form-control" value="<?php echo $project_end_date; ?>">
+                            <span class="help-block"><?php echo $project_end_date_err; ?></span>
+                        </div>
                         <div class="form-group <?php echo (!empty($project_desc)) ? 'has-error' : ''; ?>">
                             <label>Project Description</label>
-                            <input type = "text" name="project_desc" class="form-control"><?php echo $project_desc; ?></textarea>
+                            <input type = "text" name="project_desc" class="form-control"><?php echo $project_desc; ?>
                             <span class="help-block"><?php echo $project_desc_err; ?></span>
-                        </div>
-                        <div class="form-group <?php echo (!empty($public_servants_err)) ? 'has-error' : ''; ?>">
-                            <label>Public Servants</label>
-                            <input type="text" name="public_servants" class="form-control"><?php echo $public_servants; ?></textarea>
-                            <span class="help-block"><?php echo $public_servants_err; ?></span>
                         </div>
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-default">Cancel</a>
