b557866525336c06fd3fd714a31a8a7e59c82e12
IApostolou <iapostolou@athtech.gr>
2020-11-05 16:01:20 +0200

    1

---
+++ CoI/add_stakeholder.php
@@ -0,0 +1,145 @@
+<?php
+//error_reporting(0);
+// Include config file
+require_once "connect.php";
+
+// Define variables and initialize with empty values
+$stakeholder_AFM = $stakeholder_name = $stakeholder_role = $project_id = "";
+$stakeholder_AFM_err = $stakeholder_name_err = $stakeholder_role_err = $project_id_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate project name
+    $input_stakeholder_AFM= trim($_POST["stakeholder_AFM"]);
+    if (empty($input_stakeholder_AFM)) {
+        $stakeholder_AFM_err = "Please enter Stakeholder Name.";
+    } else {
+        $stakeholder_AFM = $input_stakeholder_AFM;
+    }
+
+    // Validate start date
+    $input_stakeholder_name = trim($_POST["stakeholder_name"]);
+    if (empty($input_stakeholder_name)) {
+        $stakeholder_name = "Please enter the Stakeholder name.";
+    } else {
+        $stakeholder_name = $input_stakeholder_name;
+    }
+
+    // Validate end date
+    $input_stakeholder_role = trim($_POST["stakeholder_role"]);
+    if (empty($input_stakeholder_role)) {
+        $stakeholder_role = "Please enter the Stakeholder role.";
+    } else {
+        $stakeholder_role = $input_stakeholder_role;
+    }
+
+    // Validate project_des
+    $input_project_id = trim($_POST["project_id"]);
+    if (empty($input_project_id)) {
+        $project_id_error = "Please enter the project id.";
+    } else {
+        $project_id = $input_project_id;
+    }
+
+    // // Validate usertype
+    // $input_public_servants = trim($_POST["public_servants"]);
+    // if (empty($input_public_servants)) {
+    //     $public_servants_err = "Please enter the public servants.";
+    // } else {
+    //     $public_servants = $input_public_servants;
+    // }
+
+    $stakeholder_firm = "test";
+
+    // Check input errors before inserting in database
+    if (empty($stakeholder_AFM_err) && empty($project_id_err) && empty($stakeholder_name_err) && empty($stakeholder_role_err) ){
+        // Prepare an insert statement
+        $sql = "INSERT INTO firm_stakeholder (stakeholder_AFM, stakeholder_name, stakeholder_firm, stakeholder_role, project_id) VALUES (?, ?, ?, ?, ?)";
+
+        if ($stmt = mysqli_prepare($conn, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sssss", $param_stakeholder_AFM, $param_stakeholder_name, $stakeholder_firm, $param_stakeholder_role, $param_project_id);
+
+            // Set parameters
+            $param_stakeholder_AFM = $stakeholder_AFM;
+            $param_stakeholder_name = $stakeholder_name;
+            $param_stakeholder_role = $stakeholder_role;
+            $param_project_id = $project_id;
+
+
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Records created successfully. Redirect to landing page
+                header("location: projects_FR.php");
+                exit();
+            } else {
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($conn);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style type="text/css">
+        .wrapper {
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Create Project</h2>
+                    </div>
+                    <p>Please fill this form and submit to add a new stakeholder to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($stakeholder_AFM_err)) ? 'has-error' : ''; ?>">
+                            <label>Stakeholder ΑΦΜ</label>
+                            <input type="text" name="stakeholder_AFM" class="form-control" value="<?php echo $stakeholder_AFM; ?>">
+                            <span class="help-block"><?php echo $stakeholder_AFM_err; ?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($stakeholder_name_err)) ? 'has-error' : ''; ?>">
+                            <label>Stakeholder Name</label>
+                            <input type="text" name="stakeholder_name" class="form-control" value="<?php echo $stakeholder_name; ?>">
+                            <span class="help-block"><?php echo $stakeholder_name_err; ?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($stakeholder_role_err)) ? 'has-error' : ''; ?>">
+                            <label>Stakeholder Role</label>
+                            <input type="text" name="stakeholder_role" class="form-control" value="<?php echo $stakeholder_role; ?>">
+                            <span class="help-block"><?php echo $stakeholder_role_err; ?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($project_id)) ? 'has-error' : ''; ?>">
+                            <label>Project ID</label>
+                            <input type = "text" name="project_id" class="form-control"><?php echo $project_id; ?>
+                            <span class="help-block"><?php echo $project_id_err; ?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- CoI/projects_FR.php
+++ CoI/projects_FR.php
@@ -101,19 +101,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "</td>";
                 echo "</tr>";
             }
-            /*
-            echo "</tbody>";
-            echo "<thead>";
-            echo "<tr>";
-            echo "<th>Project ID</th>";
-            echo "<th>Project Name</th>";
-            echo "<th>Project Date</th>";
-            echo "<th>Project Description</th>";
-            echo "<th>Public Servants</th>";
-            echo "</tr>";
-            echo "</thead>";
-            echo "</table>";
-            */
             echo "</table>";
 
             // Free result set
@@ -125,7 +112,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
     }
 
-    // Apply for project form
+    // Applied project list.
     echo "<form method='post'>";
     echo "<center>";
     echo "<h5 style='color:white'>Enter the Project ID you want to apply for</h5>";
@@ -135,6 +122,102 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<input type='submit' value='Apply' name='apply' class='submit-button'>" ;
     echo "</form>";
 
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    echo "<div class='page-header clearfix'>
+        <h2 class='pull-left'>Firm Projects</h2>
+    </div>";
+
+    //Table of projects the firm has applied for
+
+    $sql = "SELECT * FROM firm_project INNER JOIN projects ON firm_project.project_id = projects.project_id WHERE firm_name = 'test' ";
+    if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+            echo  "<table id='firm_projects' class='table table-striped table-sm project-table' cellspacing='0' width='100%'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th class='th-sm' id ='th-first' >Project ID</th>";
+            echo "<th class='th-sm'>Project Name</th>";
+            echo "<th class='th-sm'>Project Start Date Date</th>";
+            echo "<th class='th-sm'>Project End Date Date Date</th>";
+            echo "<th class='th-sm' id='th-last'>Project Description</th>";
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['project_id'] . "</td>";
+                echo "<td>" . $row['project_name'] . "</td>";
+                echo "<td>" . $row['project_start_date'] . "</td>";
+                echo "<td>" . $row['project_end_date'] . "</td>";
+                echo "<td>" . $row['project_desc'] . "</td>";
+                echo "<a href='update.php?id=" . $row['project_id'] . "' title='Update Record' data-toggle='tooltip'><span class='fa fa-pencil'>  </span></a>";
+                echo " ";
+                echo "<a href='delete.php?id=" . $row['project_id'] . "' title='Delete Record' data-toggle='tooltip'><span class='fa fa-trash'>  </span></a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+            // Free result set
+            mysqli_free_result($result);
+        } else {
+            echo "<p class='lead'><em>No records were found.</em></p>";
+        }
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    //Table of the firm's stakeholders
+
+    echo "<div class='page-header clearfix'>
+        <h2 class='pull-left'>Stakeholders</h2>
+    </div>";
+
+    $sql = "SELECT * FROM firm_stakeholder WHERE stakeholder_firm = 'test'";
+    if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+            echo  "<table id='stakeholders' class='table table-striped table-sm project-table' cellspacing='0' width='100%'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th class='th-sm' id ='th-first' >ΑΦΜ</th>";
+            echo "<th class='th-sm'>Name</th>";
+            echo "<th class='th-sm'>Role</th>";
+            echo "<th class='th-sm' id='th-last'>Project ID</th>";
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['stakeholder_AFM'] . "</td>";
+                echo "<td>" . $row['stakeholder_name'] . "</td>";
+                echo "<td>" . $row['stakeholder_role'] . "</td>";
+                echo "<td>" . $row['project_id'] . "</td>";
+                echo "<a href='update.php?id=" . $row['project_id'] . "' title='Update Record' data-toggle='tooltip'><span class='fa fa-pencil'>  </span></a>";
+                echo " ";
+                echo "<a href='delete.php?id=" . $row['project_id'] . "' title='Delete Record' data-toggle='tooltip'><span class='fa fa-trash'>  </span></a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+
+            // Free result set
+            mysqli_free_result($result);
+        } else {
+            echo "<p class='lead'><em>No records were found.</em></p>";
+        }
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+
+    echo "<a href='add_stakeholder.php' class='submit-button'>Add New Stakeholder</a>";
     // Close connection
     mysqli_close($conn);
     ?>
@@ -147,6 +230,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <script>
     $(document).ready(function() {
         $('#projects').DataTable();
+        $('#firm_projects').DataTable();
+        $('#stakeholders').DataTable();
         $('.dataTables_length').addClass('bs-select');
     });
 </script>
