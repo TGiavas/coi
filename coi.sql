b074d298216d29f33568cf716ad318b43c239da2
TGiavas <tgiavasoglou@gmail.com>
2020-12-03 01:39:23 +0200

    Add files via upload

--- coi.sql
+++ coi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2020 at 07:10 PM
+-- Generation Time: Dec 03, 2020 at 12:38 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -40,7 +40,8 @@ CREATE TABLE `conflicts` (
 --
 
 INSERT INTO `conflicts` (`conflict_id`, `project_id`, `ps_AFM`, `coi_description`, `status`) VALUES
-(16, 10, 123456789, 'wife on firm board', 'approved');
+(16, 10, 123456789, 'wife on firm board', 'rejected'),
+(18, 11, 144322567, 'I am project manager for this project on the firm', 'approved');
 
 -- --------------------------------------------------------
 
@@ -76,7 +77,9 @@ CREATE TABLE `firms_projects` (
 --
 
 INSERT INTO `firms_projects` (`firm_id`, `project_id`) VALUES
-(1, 10);
+(1, 10),
+(1, 13),
+(1, 12);
 
 -- --------------------------------------------------------
 
@@ -99,7 +102,7 @@ CREATE TABLE `firm_stakeholders` (
   `stakeholder_AFM` int(9) NOT NULL,
   `stakeholder_name` varchar(50) NOT NULL,
   `firm_id` int(11) NOT NULL,
-  `stakeholder_role` varchar(20) NOT NULL,
+  `stakeholder_role` varchar(50) NOT NULL,
   `project_id` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -108,6 +111,7 @@ CREATE TABLE `firm_stakeholders` (
 --
 
 INSERT INTO `firm_stakeholders` (`stakeholder_AFM`, `stakeholder_name`, `firm_id`, `stakeholder_role`, `project_id`) VALUES
+(233145666, 'Pavlos Diogenous', 1, 'Shareholder in the company', 13),
 (555111333, 'Giannis Sxinas', 1, 'board director', 10);
 
 -- --------------------------------------------------------
@@ -129,7 +133,13 @@ CREATE TABLE `projects` (
 --
 
 INSERT INTO `projects` (`project_id`, `project_name`, `project_start_date`, `project_end_date`, `project_desc`) VALUES
-(10, 'Egnatia Odos', '2020-11-25', '2021-06-08', 'Build public road in Macedonia');
+(10, 'Egnatia Odos', '2020-11-25', '2021-06-08', 'Build public road in Macedonia'),
+(11, '5G Network', '2020-11-17', '2020-12-31', 'Improve network capabilities to include 5G'),
+(12, 'Replace asphalt', '2020-11-18', '2021-04-19', 'New asphalting of public road in Marousi Attikis'),
+(13, 'Megalos Peripatos', '2020-11-16', '2021-05-25', 'Create sidewalk in Athens center'),
+(14, 'Christmas decoration', '2020-12-03', '2021-01-26', 'Christmas decoration for Athens center'),
+(15, 'Public square renovation', '2020-12-03', '2021-06-18', 'Public square renovation in village Karydia Aitoloakarnanias'),
+(16, 'New military aircraft', '2020-11-11', '2021-12-22', 'Create new military aircraft for the army');
 
 -- --------------------------------------------------------
 
@@ -148,7 +158,8 @@ CREATE TABLE `public_servants` (
 --
 
 INSERT INTO `public_servants` (`ps_AFM`, `project_id`, `ps_role`) VALUES
-(123456789, 10, 'technical overseer');
+(123456789, 10, 'technical overseer'),
+(144322567, 11, 'economical overseer');
 
 -- --------------------------------------------------------
 
@@ -181,6 +192,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`AFM`, `name`, `password`, `user_type`, `created_at`) VALUES
 (123456789, 'Kostas Petrakopoulos', '$2y$10$F./KHQ12KSxoJsP4jXMYueDO3CKWQ9sYxFN2s3IpHow/QthkSHrzq', 'PS', '2020-12-02 19:00:24'),
+(144322567, 'Ioannis Ioannou', '$2y$10$Se6mDR8UeQbNWhDf0I6z1uuDsd0y9SRHmFQ7jlfK78j4QLVVeAg9.', 'PS', '2020-12-03 01:05:22'),
 (155432111, 'Giannis Papadopoulos', '$2y$10$uIQVfiYUos87HBERGdPPyOlJZzFovf4e4bsELtVGSZlKB8xtubNY.', 'FR', '2020-12-02 18:49:59'),
 (156432111, 'Orestis Kanakis', '$2y$10$FeN1bcjnHxb/05JmLseE/.hes1FR1vk9UMGoC5xVNd4GrC/nd8y0q', 'FR', '2020-12-02 19:29:56'),
 (167222135, 'Thanos Giannakis', '$2y$10$XJj.JD60361vyy5r8Ke2heHoFmtkT2r.lqoZU.XNAxkOH54nFooZG', 'GO', '2020-12-02 19:32:43');
@@ -261,7 +273,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `conflicts`
 --
 ALTER TABLE `conflicts`
-  MODIFY `conflict_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `conflict_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `firm`
@@ -273,7 +285,7 @@ ALTER TABLE `firm`
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `project_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
