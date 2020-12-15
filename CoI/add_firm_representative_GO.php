ad9c0ecb88379c08acc47aaf4ddb4db05759ab97
TGiavas <tgiavasoglou@gmail.com>
2020-12-15 19:29:25 +0200

    Add files via upload

---
+++ CoI/add_firm_representative_GO.php
@@ -0,0 +1,138 @@
+<?php include('header.php');
+include('connect.php');
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+
+<?php
+
+
+// Define variables and initialize with empty values
+$fr_AFM = $firm_id = "";
+$fr_AFM_err = $firm_id_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate project name
+    if (isset($_POST["fr_AFM"]) && !empty($_POST["fr_AFM"])) {
+        // Get hidden input value
+        $fr_AFM = $_POST["fr_AFM"];
+    }
+
+
+    // Validate project_des
+    if (isset($_POST["firm_id"]) && !empty($_POST["firm_id"])) {
+        // Get hidden input value
+        $firm_id = $_POST["firm_id"];
+    }
+
+    // // Validate usertype
+    // $input_public_servants = trim($_POST["public_servants"]);
+    // if (empty($input_public_servants)) {
+    //     $public_servants_err = "Please enter the public servants.";
+    // } else {
+    //     $public_servants = $input_public_servants;
+    // }
+
+
+    // Check input errors before inserting in database
+    if (empty($fr_AFM_err) && empty($fr_id_err)) {
+        // Prepare an insert statement
+        $sql = "INSERT INTO firm_representatives (fr_AFM, firm_id) VALUES (?, ?)";
+
+        if ($stmt = mysqli_prepare($conn, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_fr_AFM, $param_firm_id);
+
+            // Set parameters
+
+            $param_fr_AFM = $fr_AFM;
+            $param_firm_id = $firm_id;
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Records created successfully. Redirect to landing page
+                header("location:view_firm_representatives_GO.php");
+                exit();
+            } else {
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($conn);
+}
+?>
+
+
+<?php include('navbar_GO.php')?>
+<style type="text/css">
+    .wrapper {
+        width: 500px;
+        margin: 0 auto;
+    }
+</style>
+
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Add Firm Representative</h2>
+                    </div>
+                    <p>Please fill this form and submit to add a new firm representative to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <?php
+                        $sql = "SELECT * FROM users WHERE user_type = 'FR' ORDER BY name";
+                        $result = $conn->query($sql); ?>
+                        <div class="form-group <?php echo (!empty($fr_AFM_err)) ? 'has-error' : ''; ?>">
+                            <label>Firm Representative Name</label>
+                            <select class="browser-default custom-select" name='fr_AFM'>
+                                <?php while ($rows = $result->fetch_assoc()) {
+                                    $fr_name = $rows['name'];
+                                    $fr_AFM = $rows['AFM'];
+                                    echo ("<option value='$fr_AFM'>$fr_name</option>");
+                                } ?>
+                            </select>
+                        </div>
+
+                        <?php
+                        $sql = "SELECT * FROM firms";
+                        $result = $conn->query($sql); ?>
+                        <div class="form-group <?php echo (!empty($fr_AFM_err)) ? 'has-error' : ''; ?>">
+                            <label>Firm</label>
+                            <select class="browser-default custom-select" name='firm_id'>
+                                <?php while ($rows = $result->fetch_assoc()) {
+                                    $firm_name = $rows['firm_name'];
+                                    $firm_id = $rows['firm_id'];
+                                    echo ("<option value='$firm_id'>$firm_name</option>");
+                                } ?>
+                            </select>
+                        </div>
+                        <!-- <div class="form-group <?php echo (!empty($firm_id)) ? 'has-error' : ''; ?>">
+                            <label>Project ID</label>
+                            <input type="text" name="firm_id" class="form-control"><?php echo $firm_id; ?>
+                            <span class="help-block"><?php echo $fr_AFM_err; ?></span>
+                        </div> -->
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="projects_FR.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('footer.php') ?>
+
+
+
+</body>
+
+</html>
---
+++ CoI/view_firm_representatives_GO.php
@@ -0,0 +1,61 @@
+<?php include('header.php');
+include('connect.php');
+require_once "config.php";
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+include('navbar_GO.php')
+?>
+
+
+<div id="Public Servants">
+    <div class="wrapper-table">
+        <div class="page-header clearfix">
+            <h2 class="pull-left">Firm representatives</h2>
+        </div>
+        <?php
+        // Attempt select query execution
+        $sql = "SELECT * FROM firm_representatives INNER JOIN users ON users.AFM = firm_representatives.fr_AFM INNER JOIN firms ON firm_representatives.firm_id = firms.firm_id";
+        if ($result = mysqli_query($conn, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+                echo  "<table id='firm_representatives' class='table table-striped table-sm project-table' cellspacing='0' width='100%'>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th class='th-sm' id ='th-first'>Firm Representative ΑΦΜ</th>";
+                echo "<th class='th-sm'>Firm Representative Name</th>";
+                echo "<th class='th-sm'>Firm Name</th>";
+                /*echo "<th class='th-sm' id ='th-last' style='width: 5%'>Action</th>";*/
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                while ($row = mysqli_fetch_array($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['fr_AFM'] . "</td>";
+                    echo "<td>" . $row['name'] . "</td>";
+                    echo "<td>" . $row['firm_name'] . "</td>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+                mysqli_free_result($result);
+            } else {
+                echo "<p class='lead'><em>No records were found.</em></p>";
+            }
+        } else {
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+        ?>
+        </table>
+    </div>
+</div>
+
+</body>
+<?php include('footer.php') ?>
+
+<script>
+    $(document).ready(function() {
+        $('#firm_representatives').DataTable();
+    });
+</script>
+</html>
