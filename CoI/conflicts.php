2e119bcd36ae3335adcb3741d9c01b38b82bae22
TGiavas <tgiavasoglou@gmail.com>
2020-12-15 19:40:04 +0200

    Add files via upload

--- CoI/conflicts.php
+++ CoI/conflicts.php
@@ -3,59 +3,10 @@ include('connect.php');
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+include('navbar_GO.php')
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Conflicts of Interest</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <span class="navbar-text">
-        Government Official:
-        <?php echo $_SESSION["AFM"]; ?>
-    </span>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Projects
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="projects_GO.php">View Projects</a>
-                    <a class="dropdown-item" href="create_project.php">Create New Project</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="conflicts.php">Conflicts of Interest</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="register.php">Register User</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Public Servants
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="view_public_servants_GO.php">View Pulic Servants</a>
-                    <a class="dropdown-item" href="add_public_servant_GO.php">Add New Public Servant</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Firm Representatives
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="view_firm_representatives_GO.php">View Firm Representatives</a>
-                    <a class="dropdown-item" href="add_firm_representative_GO.php">Add New Firm Representative</a>
-                </div>
-            </li>
 
-        </ul>
-        <form class="form-inline my-2 my-lg-0" method="post" action="logout.php" id="logoutform">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="logout-btn">Log Out</button>
-        </form>
-    </div>
-</nav>
 
 <div id="Conflicts">
     <div class="wrapper-table">
