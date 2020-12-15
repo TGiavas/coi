ad905647ef87e6e657adbc58643edc8f2b427315
TGiavas <tgiavasoglou@gmail.com>
2020-12-15 19:25:10 +0200

    Add files via upload

---
+++ CoI/add_firm_GO.php
@@ -0,0 +1,120 @@
+<?php include('header.php');
+include('connect.php');
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+?>
+
+
+<?php
+
+
+// Define variables and initialize with empty values
+$firm_name = "";
+$firm_name_err = "";
+
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $input_firm_name = trim($_POST["firm_name"]);
+    if (empty($input_firm_name)) {
+        $firm_name_err = "Please enter a project name.";
+    } else {
+        $firm_name = $input_firm_name;
+    }
+
+
+
+    // // Validate usertype
+    // $input_public_servants = trim($_POST["public_servants"]);
+    // if (empty($input_public_servants)) {
+    //     $public_servants_err = "Please enter the public servants.";
+    // } else {
+    //     $public_servants = $input_public_servants;
+    // }
+
+
+    // Check input errors before inserting in database
+    if (empty($firm_name_err)) {
+        // Prepare an insert statement
+        $sql = "INSERT INTO firms (firm_name) VALUES(?)";
+
+        if ($stmt = mysqli_prepare($conn, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $firm_name);
+
+            // Set parameters
+
+            $param_firm_name = $firm_name;
+
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Records created successfully. Redirect to landing page
+                header("location:view_firms_GO.php");
+                exit();
+            } else {
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($conn);
+}
+?>
+
+<?php include('navbar_GO.php') ?>
+
+<style type="text/css">
+    .wrapper {
+        width: 500px;
+        margin: 0 auto;
+    }
+</style>
+
+
+
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Add Firm</h2>
+                    </div>
+                    <p>Please fill this form and submit to add a new firm representative to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($firm_name_err)) ? 'has-error' : ''; ?>">
+                            <label>Firm Name</label>
+                            <input type="text" name="firm_name" class="form-control" value="<?php echo $firm_name; ?>">
+                            <span class="help-block"><?php echo $firm_name_err; ?></span>
+                        </div>
+
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="projects_FR.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+<?php include('footer.php') ?>
+
+
+
+</body>
+
+</html>
---
+++ CoI/view_firms_GO.php
@@ -0,0 +1,58 @@
+<?php include('header.php');
+include('connect.php');
+require_once "config.php";
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+include('navbar_GO.php')
+?>
+
+
+<div id="Firms">
+    <div class="wrapper-table">
+        <div class="page-header clearfix">
+            <h2 class="pull-left">Firm representatives</h2>
+        </div>
+        <?php
+        // Attempt select query execution
+        $sql = "SELECT * FROM firms";
+        if ($result = mysqli_query($conn, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+                echo  "<table id='firms' class='table table-striped table-sm project-table' cellspacing='0' width='100%'>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th class='th-sm' id ='th-first'>Firm Name</th>";
+                /*echo "<th class='th-sm' id ='th-last' style='width: 5%'>Action</th>";*/
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                while ($row = mysqli_fetch_array($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['firm_name'] . "</td>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+                mysqli_free_result($result);
+            } else {
+                echo "<p class='lead'><em>No records were found.</em></p>";
+            }
+        } else {
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+        }
+        echo "<a href='add_firm_GO.php' class='submit-button'>Add New Firm</a>";
+        mysqli_close($conn);
+        ?>
+        </table>
+    </div>
+</div>
+
+</body>
+<?php include('footer.php') ?>
+
+<script>
+    $(document).ready(function() {
+        $('#firms').DataTable();
+    });
+</script>
+</html>
