6c0db21bdb2bb24371045219aeb80e33e29f71bb
TGiavas <tgiavasoglou@gmail.com>
2020-12-16 16:27:37 +0200

    Add files via upload

--- CoI/applied_projects_FR.php
+++ CoI/applied_projects_FR.php
@@ -1,44 +1,10 @@
-
 <?php
 include('header.php');
 include('connect.php');
+include('navbar_FR.php');
+include('redirect_FR.php');
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Conflicts of Interest</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <span class="navbar-text">
-        Firm Representative:
-        <?php echo $_SESSION["AFM"]; ?>
-    </span>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Projects
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="projects_FR.php">View All Projects</a>
-                    <a class="dropdown-item" href="applied_projects_FR.php">View Applied Projects</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Stakeholders
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="firm_stakeholders.php">View Firm Stakeholders</a>
-                    <a class="dropdown-item" href="add_stakeholder.php">Add New Stakeholder</a>
-                </div>
-            </li>
 
-        </ul>
-        <form class="form-inline my-2 my-lg-0" method="post" action="logout.php" id="logoutform">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="logout-btn">Log Out</button>
-        </form>
-    </div>
-</nav>
 
 
 <div id="View Applied Projects" class="tabcontent">
@@ -48,7 +14,13 @@ include('connect.php');
     </div>
     <?php
 
-    $sql = "SELECT * FROM firms_projects INNER JOIN projects ON firms_projects.project_id = projects.project_id WHERE firm_id = 1 ";
+    $sql = "SELECT * FROM users INNER JOIN firm_representatives ON users.AFM = firm_representatives.fr_AFM";
+    $result = $conn->query($sql);
+    $firm_id = "";
+    while ($rows = $result->fetch_assoc()) {
+      $firm_id = $rows['firm_id'];
+    }
+    $sql = "SELECT * FROM firms_projects INNER JOIN projects ON firms_projects.project_id = projects.project_id WHERE firm_id = $firm_id ";
     if ($result = mysqli_query($conn, $sql)) {
       if (mysqli_num_rows($result) > 0) {
         echo  "<table id='firm_projects' class='table table-striped table-sm project-table' cellspacing='0' width='100%'>";
@@ -91,10 +63,10 @@ include('connect.php');
 
 <?php include('footer.php') ?>
 <script>
-    $(document).ready(function() {
-        $('#firm_projects').DataTable();
-        $('.dataTables_length').addClass('bs-select');
-    });
+  $(document).ready(function() {
+    $('#firm_projects').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+  });
 </script>
 
 </body>
--- CoI/firm_stakeholders.php
+++ CoI/firm_stakeholders.php
@@ -2,43 +2,10 @@
 <?php
 include('header.php');
 include('connect.php');
+include('navbar_FR.php');
+include('redirect_FR.php');
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Conflicts of Interest</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <span class="navbar-text">
-        Firm Representative:
-        <?php echo $_SESSION["AFM"]; ?>
-    </span>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Projects
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="projects_FR.php">View All Projects</a>
-                    <a class="dropdown-item" href="applied_projects_FR.php">View Applied Projects</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Stakeholders
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="firm_stakeholders.php">View Firm Stakeholders</a>
-                    <a class="dropdown-item" href="add_stakeholder.php">Add New Stakeholder</a>
-                </div>
-            </li>
 
-        </ul>
-        <form class="form-inline my-2 my-lg-0" method="post" action="logout.php" id="logoutform">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="logout-btn">Log Out</button>
-        </form>
-    </div>
-</nav>
 
 
 <div id="View Firm Stakeholders" class="tabcontent">
@@ -46,7 +13,15 @@ include('connect.php');
     <div class="page-header clearfix">
       <h2 class="pull-left">Firm Stakholders</h2>
     </div>
-    <?php $sql = "SELECT * FROM firm_stakeholders";
+    <?php 
+    $sql = "SELECT * FROM users INNER JOIN firm_representatives ON users.AFM = firm_representatives.fr_AFM";
+    $result = $conn->query($sql); 
+    $firm_id = "";
+    while ($rows = $result->fetch_assoc()) {
+      $firm_id = $rows['firm_id'];
+    }
+    
+    $sql = "SELECT * FROM firm_stakeholders WHERE firm_id=$firm_id";
     if ($result = mysqli_query($conn, $sql)) {
       if (mysqli_num_rows($result) > 0) {
         echo  "<table id='stakeholders' class='table table-striped table-sm project-table' cellspacing='0' width='100%'>";
