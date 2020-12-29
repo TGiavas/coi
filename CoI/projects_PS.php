2015739e8a6d8c16dc6fa5d14f40fe03371c284a
Themis Giavasoglou <tgiavasoglou@gmail.com>
2020-12-29 23:47:46 +0200

    updated public servant projects with extra column to show if he had declared a conflict of interest on project

--- CoI/projects_PS.php
+++ CoI/projects_PS.php
@@ -14,8 +14,8 @@ include('navbar_PS.php');
         <?php
         // Attempt select query execution
         $AFM = $_SESSION['AFM'];
-        $sql = "SELECT DISTINCT projects.project_id, projects.project_name, projects.project_start_date, projects.project_end_date, projects.project_desc
-        FROM public_servants INNER JOIN projects ON projects.project_id = public_servants.project_id  WHERE public_servants.ps_AFM = $AFM";
+        $sql = "SELECT projects.project_id, projects.project_name, projects.project_start_date, projects.project_end_date, projects.project_desc, conflicts.coi_description
+        FROM projects LEFT JOIN public_servants ON projects.project_id = public_servants.project_id LEFT JOIN conflicts ON projects.project_id = conflicts.project_id  WHERE public_servants.ps_AFM = $AFM";
         if ($result = mysqli_query($conn, $sql)) {
             if (mysqli_num_rows($result) > 0) {
                 echo  "<table id='ps_projects' class='table table-striped table-sm project-table' cellspacing='0' width='100%'>";
@@ -26,6 +26,7 @@ include('navbar_PS.php');
                 echo "<th class='th-sm'>Project Start Date </th>";
                 echo "<th class='th-sm'>Project End Date </th>";
                 echo "<th class='th-sm'>Project Description</th>";
+                echo "<th class='th-sm' id='th-last'>Conflict Description</th>";
 
                 /*echo "<th class='th-sm' id ='th-last' style='width: 5%'>Action</th>";*/
                 echo "</tr>";
@@ -38,6 +39,7 @@ include('navbar_PS.php');
                     echo "<td>" . $row['project_start_date'] . "</td>";
                     echo "<td>" . $row['project_end_date'] . "</td>";
                     echo "<td>" . $row['project_desc'] . "</td>";
+                    echo "<td>" . $row['coi_description'] . "</td>";
                     echo "</td>";
                     echo "</tr>";
                 }
